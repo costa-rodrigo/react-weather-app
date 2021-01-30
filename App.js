670ba2954c395e20a906d43d1aac2a8aea631add
Rodrigo <rodrigo.lor@gmail.com>
2021-01-30 12:15:29 -0800

    Final Version

--- App.js
+++ App.js
@@ -1,14 +1,66 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Content from './components/Content'
+import Footer from './components/Footer';
+import Form from './components/Form'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const API_KEY = '4440de8f4173e3ce9571ace93718f1d0'
+
+class App extends Component {
+  state = {
+    temp: '',
+    windSpeed: '',
+    pressure: '',
+    minTemp: '',
+    city: '',
+    countryCode: ''
+  }
+
+  fetchData = () => {
+    this.fetchCityData(this.state.city)
+  }
+
+  fetchCityData = async city => {
+    const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
+    const api_call = await fetch(url)
+
+    const response = await api_call.json()
+    // console.log('response', response)
+
+    this.setState({
+      temp: response.main.temp,
+      windSpeed: response.wind.speed,
+      minTemp: response.main.temp_min,
+      pressure: response.main.pressure,
+      countryCode: response.sys.country,
+      weather: response.weather[0].main
+    })
+  }
+
+  render () {
+    // console.log('current text', this.state.city)
+    const { city, countryCode, temp, minTemp, windSpeed, pressure, weather } = this.state
+    return (
+      <View style={styles.container}>
+        <Form 
+          onSubmit={this.fetchData}
+          onChangeText={text => this.setState({ city: text })}
+        />
+        <Content 
+          temp={temp}
+          city={city}
+          countryCode={countryCode}
+          weather={weather}
+        />
+        <Footer 
+          pressure={pressure}
+          windSpeed={windSpeed}
+          minTemp={minTemp}
+        />
+      </View>
+    )
+  }
 }
 
 const styles = StyleSheet.create({
@@ -19,3 +71,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+export default App
